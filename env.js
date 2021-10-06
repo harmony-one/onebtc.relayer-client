f8df4fed2b310b5a9250d989e0557dd34d80a261
yuriy <yuriy.menkov@latoken.com>
2021-10-06 12:19:01 +0300

    v1.0.0

--- env.js
+++ env.js
@@ -6,7 +6,7 @@ const path = require('path');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-const NODE_ENV = process.env.NODE_ENV || 'mainnet';
+const NODE_ENV = process.env.NODE_ENV || 'testnet';
 
 if (!process.env.NODE_ENV) {
   console.warn('NODE ENV is not specified. Set to production');
